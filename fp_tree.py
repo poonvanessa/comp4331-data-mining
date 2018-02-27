b98e1e760002df346302975646d020f47aa2c868
poonvanessa <iamvanessapoon@gmail.com>
2018-02-27 16:18:23 +0800

    complete constructing fp-tree

--- fp_tree.py
+++ fp_tree.py
@@ -3,21 +3,24 @@
 data = [ '25', '52', '164', '240', '274', '328', '368', '448', '538', '561', '630', '687', '730', '775', '825', '834' ]
 
 minsup = 5
-rawDict = {}
+freqItemsDict = {}  # {'3': 6, '2': 6, '5': 6, '4': 6, '8': 7}  only those above the threshold
+rawDict = {}    # { frozenset(['2','5']): 2, frozenset(['1','4', '6']): 1 ... }
 
+# prepare data
 for trans in data:
-    items = list(trans)
-    for item in items:
-        rawDict[item] = rawDict.get(item, 0) + 1
-# rawDict => {'1': 2, '0': 3, '3': 6, '2': 6, '5': 6, '4': 6, '7': 5, '6': 5, '8': 7}
+    print list(trans)
+    # print "trans" + list(trans) + str(type(trans))
+    rawDict[tuple(trans)] = rawDict.get(tuple(trans), 0) + 1
+    for item in trans:
+        freqItemsDict[item] = freqItemsDict.get(item, 0) + 1
 
-for key in rawDict.keys():
-    if rawDict[key] <= minsup :
-        del rawDict[key]    
-# rawDict => {'3': 6, '2': 6, '5': 6, '4': 6, '8': 7}
+# filter out candidate of length 1 which are below threshold
+for key in freqItemsDict.keys():
+    if freqItemsDict[key] <= minsup :
+        del freqItemsDict[key]    
 
 # TODO1: sort rawDict by key alphabetically and then value
-orderedFreqItems = [v[0] for v in sorted(sorted(rawDict.items(), key=lambda q:q[0], reverse=False), key=lambda p: p[1], reverse=True)]
+orderedFreqItemsBySupport = [v[0] for v in sorted(sorted(freqItemsDict.items(), key=lambda q:q[0], reverse=False), key=lambda p: p[1], reverse=True)]
 # orderedFreqItems => ['8', '2', '3', '4', '5']
 
 # TODO2: create class for 'tree'
@@ -33,24 +36,54 @@ class tree:
             return self.itemName
         return " - "
 
-    def toString(self):
-        print "Name-" + self.itemName + " : count-" + str(self.count)
+    def toString(self, indentation):
+        print indentation + "Name-" + self.itemName + " : count-" + str(self.count)
         #  + " : Parent-" + self.parent.itemName
-        # print children
+        # print self.children
         if self.children:
             # print self.children 
             for childName in self.children:
-                print childName 
+                # print childName +" : "+str(self.children[childName].count)
+                self.children[childName].toString(indentation + "-")
+
+    def inc(self,counter):
+        self.count = self.count + counter
+
+# accept a list of frequent items and update the fp-tree
+def updateTree(items, parentTree, count):
+    print "updateTree is called - "+parentTree.getName()+" : "+str(count)
+    print items
+    print "------------------------------------------------"
+    if items[0] in parentTree.children:
+        parentTree.children[items[0]].inc(count)
+    else:
+        parentTree.children[items[0]] = tree(items[0], parentTree, count)
+    if len(items) > 1:
+        print "sub update"
+        print items[1::]
+        print parentTree.children[items[0]].getName()
+        updateTree(items[1::], parentTree.children[items[0]], count)
 
 # TODO3: create fp-tree based on database
 # TODO4: create conditional fp-tree based on fp-tree
-print orderedFreqItems
+
 root = tree('root', None, 0)
-
 # incorrect: create treeNode for all frequent items and make them as child of 'root'
-for item in orderedFreqItems:
-    root.children[item] = tree(item, root, 0)
+for trans, count in rawDict.iteritems():
+    # print list(trans) # frozenset(['0', '3', '6'])
+    orderedItems = []
+    for item in trans:
+        if item in freqItemsDict.keys():
+            orderedItems.append(item)
+    print trans
+    print "sorted orderedItems :"
+    print sorted(orderedItems)
+    # updating the tree
+    updateTree(sorted(orderedItems), root, count)        
+# for item in orderedFreqItemsBySupport:
+#     root.children[item] = tree(item, root, 0)
+root.toString("-")
 
-root.toString()
+# orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
 
 
